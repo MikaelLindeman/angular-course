eb6fba2da59d1b7d9afad7507649ff372ddb91b1
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2022-12-06 14:38:26 +0100

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template:
+ /* template:
     `<app-server></app-server>
-     <app-server></app-server>`,
+     <app-server></app-server>`, */
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
 
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
